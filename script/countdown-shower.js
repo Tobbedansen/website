1fe03df482f264d15f1b6bcea59f6dd3fdbbfa61
Jonas-D-M <jonas.de.meyer@student.howest.be>
2022-06-24 11:51:10 +0200

    make coutndown appear dynamic

--- script/countdown-shower.js
+++ script/countdown-shower.js
@@ -1,17 +1,57 @@
-// TODO: look into: https://www.cssscript.com/flipping-countdown-clock-javascript-css-countdown-js/
+const shouldShowCountdown = () => {
+  const currentDate = new Date();
+  const currentMonth = currentDate.getMonth();
+
+  const upperLimitMonth = 2;
+
+  if (currentMonth >= upperLimitMonth || currentDate >= getStartingDate()) {
+    return true;
+  }
+  return false;
+};
+
+const getStartingDate = () => {
+  const year = new Date().getFullYear();
+  const month = 8; // september
+
+  const daysInMonth = new Date(year, month, 0).getDate();
+
+  for (let i = 1; i <= daysInMonth; i++) {
+    let newDate = new Date(year, month, i);
+
+    if (newDate.getDay() === 0) {
+      //Sunday
+      return new Date(year, month, i - 2, (hours = 19));
+      break;
+    }
+    if (newDate.getDay() === 6) {
+      // Saturday
+      return new Date(year, month, i - 1, (hours = 19));
+    }
+  }
+};
+
+const createCountdownDate = (startingDate) => {
+  const dateObj = {
+    day: startingDate.getDate(),
+    month: startingDate.getMonth() + 1,
+    year: startingDate.getFullYear(),
+    hour: startingDate.getHours(),
+    minute: 0,
+    second: 0,
+  };
+  console.log(dateObj);
+  return dateObj;
+};
 
 document.addEventListener('DOMContentLoaded', function () {
+  const tobbedansenDate = getStartingDate();
+  console.log(tobbedansenDate);
+
   let cd = new Countdown({
     cont: document.querySelector('.container'),
     countdown: true,
-    date: {
-      day: 2,
-      month: 9,
-      year: 2022,
-      hour: 18,
-      minute: 0,
-      second: 0,
-    },
+    date: createCountdownDate(tobbedansenDate),
     outputTranslation: {
       year: 'Years',
       week: 'Weken',
@@ -24,46 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
     outputFormat: 'day|hour|minute|second',
   });
 
-  cd.start();
+  if (shouldShowCountdown()) {
+    cd.start();
+  }
 });
-
-// var cd = new Countdown({
-//     cont: null,
-//     countdown: true,
-//     endDate: {
-//       year: 0,
-//       month: 0,
-//       day: 0,
-//       hour: 0,
-//       minute: 0,
-//       second: 0
-//     },
-//     endCallback: null,
-//     outputFormat: 'year|week|day|hour|minute|second',
-//     outputTranslation: {
-//       year: 'Years',
-//       week: 'Weeks',
-//       day: 'Days',
-//       hour: 'Hours',
-//       minute: 'Minutes',
-//       second: 'Seconds',
-//     }
-// });
-
-// var countDownDate = new Date('Sep 3, 2021 18:00:00').getTime();
-// var x = setInterval(function () {
-//   var now = new Date().getTime();
-//   var distance = countDownDate - now;
-//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-//   document.getElementById('countdown').innerHTML = `<div id="days" class="countdown-item">${days}d</div>
-//   <div id="hours" class="countdown-item">${hours}h</div>
-//   <div id="minutes" class="countdown-item">${minutes}m</div>
-//   <div id="seconds" class="countdown-item">${seconds}s</div>`;
-//   if (distance < 0) {
-//     clearInterval(x);
-//     document.getElementById('countdown').innerHTML = '';
-//   }
-// });
